eccd9bcf63103209f9fe74f980d24659bcb11dab
Furkan Aslanmirza <95697472+FurkanAslanmirza@users.noreply.github.com>
2023-06-26 17:07:05 +0200

    Test kommentiert

--- DocumentManagementSystem/src/DocumentManagementSystem/Datenbank/DocumentTest.java
+++ DocumentManagementSystem/src/DocumentManagementSystem/Datenbank/DocumentTest.java
@@ -5,6 +5,7 @@ import DocumentManagementSystem.GUI.GUI;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -17,8 +18,11 @@ public class DocumentTest {
     void testSaveUploadDocument() {
         DocumentManager docmanger = new DocumentManager();
         GUI ui = new GUI(docmanger);
+        String pfad = JOptionPane.showInputDialog("Bitte Pfad einfügen");
 
-        assertTrue(ui.saveUploadDocument("C:\\Users\\Furka\\Desktop\\abc.txt"));
+        //prüft die Methode in der GUI
+        assertTrue(ui.saveUploadDocument(pfad));
+        //prüft, ob die gespeicherte Datei nun wirklich existiert
         assertTrue(Files.exists(Paths.get("DocumentManagementSystem/SaveFile")));
     }
 }
